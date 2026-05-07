8787fbab337a381ca3a3ae34b7444d9cdf103bd7
Karol Ostrowski <xlolsonx@gmail.com>
2026-05-07 13:13:13 +0200

    Fix problem with simple returns not inserting end

--- queries/crystal/endwise.scm
+++ queries/crystal/endwise.scm
@@ -7,7 +7,7 @@
 ((annotation_def name: (_) @cursor) @endable @indent (#endwise! "end"))
 ((c_struct_def name: (_) @cursor) @endable @indent (#endwise! "end"))
 
-((method_def name: (_) @cursor params: (param_list)? @cursor type: (_)? ")"? @cursor) @endable @indent (#endwise! "end"))
+((method_def name: (_) @cursor params: (param_list)? @cursor type: (_)? @cursor ")"? @cursor) @endable @indent (#endwise! "end"))
 ((macro_def name: (_) @cursor params: (param_list)? ")"? @cursor) @endable @indent (#endwise! "end"))
 ((fun_def name: (_) @cursor params: (param_list)? ")"? @cursor type: (_)? @cursor) @endable @indent (#endwise! "end"))
 
